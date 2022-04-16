af958fe9ea6216dd921eebcfca18892227352006
Burak-Tasci <buraaktascii@outlook.com>
2022-04-16 19:42:08 +0300

    create | settings screen ui

---
+++ app/src/main/java/com/tsci/electioncountdown/presentation/settings/SettingsFragment.kt
@@ -0,0 +1,26 @@
+package com.tsci.electioncountdown.presentation.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tsci.electioncountdown.R
+import dagger.hilt.android.AndroidEntryPoint
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.settings_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
