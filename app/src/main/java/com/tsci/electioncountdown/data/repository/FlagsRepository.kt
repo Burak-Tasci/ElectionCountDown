1bdc6e52b876acf7217c7da072114a7e12912ebc
Burak-Tasci <buraaktascii@outlook.com>
2022-04-16 19:44:14 +0300

    create | repository

---
+++ app/src/main/java/com/tsci/electioncountdown/data/repository/FlagsRepository.kt
@@ -0,0 +1,17 @@
+package com.tsci.electioncountdown.data.repository
+
+import androidx.annotation.StringRes
+import com.tsci.electioncountdown.data.model.CountryItem
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FlagsRepository {
+
+    suspend fun getCountryById(id: String): CountryItem
+
+    suspend fun getCountryByName(name: String): CountryItem
+
+    suspend fun getFlagUrlByCountryName(name: String): String
+
+    suspend fun getFlagUrlByCountryId(id: String): String
+}
