8f74e3e531f3b7bf5942639393d8fe8957f4bb95
Burak-Tasci <buraaktascii@outlook.com>
2022-04-19 14:00:29 +0300

    add | supress warning

--- app/src/main/java/com/tsci/electioncountdown/presentation/SplashScreenActivity.kt
+++ app/src/main/java/com/tsci/electioncountdown/presentation/SplashScreenActivity.kt
@@ -1,11 +1,13 @@
 package com.tsci.electioncountdown.presentation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
     private val handler = Handler()
