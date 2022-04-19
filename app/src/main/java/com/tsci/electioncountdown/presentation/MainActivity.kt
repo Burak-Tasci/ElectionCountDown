6ac4df8d2ab7c44a7b8ca5768c7331049791206f
Burak-Tasci <buraaktascii@outlook.com>
2022-04-19 13:59:05 +0300

    add | log tag

--- app/src/main/java/com/tsci/electioncountdown/presentation/MainActivity.kt
+++ app/src/main/java/com/tsci/electioncountdown/presentation/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.tsci.electioncountdown.presentation
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.tsci.electioncountdown.R
 import dagger.hilt.android.AndroidEntryPoint
 
+
+const val TAG = "MainActivity.kt"
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu,menu)
@@ -25,4 +33,6 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
 }
