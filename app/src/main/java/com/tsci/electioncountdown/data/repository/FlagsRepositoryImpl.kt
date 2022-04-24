5002e9d214a32ad5e1679a46330f25335b2ff7e1
Burak-Tasci <buraaktascii@outlook.com>
2022-04-25 00:11:04 +0300

    add | getCountryByCode func

--- app/src/main/java/com/tsci/electioncountdown/data/repository/FlagsRepositoryImpl.kt
+++ app/src/main/java/com/tsci/electioncountdown/data/repository/FlagsRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.tsci.electioncountdown.data.repository
 
 import android.content.res.Resources
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.google.gson.JsonObject
 import com.tsci.electioncountdown.common.Constants.BASE_URL
 import com.tsci.electioncountdown.data.model.CountryItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import java.net.URL
 import javax.inject.Inject
 
@@ -18,11 +22,18 @@ class FlagsRepositoryImpl @Inject constructor(
         throw Resources.NotFoundException("Country Not Found!")
     }
 
-    override suspend fun getCountryByName(name: String): CountryItem? {
+    override fun getCountryByName(name: String): Flow<CountryItem?> {
         database.forEach {
-            if(it.country.equals(name)) return it
+            if(it.country.equals(name)) return flowOf(it)
         }
-        return null
+        return flowOf(null)
+    }
+
+    override fun getCountryByCode(code: String): Flow<CountryItem?> {
+        database.forEach {
+            if (it.countryCode.uppercase().equals(code)) return flowOf(it)
+        }
+        return flowOf(null)
     }
 
     override suspend fun getFlagUrlByCountryName(name: String): String {
