f3670ef05c8b9b3f907c5b2ae155e7b937b612ae
Burak-Tasci <buraaktascii@outlook.com>
2022-04-28 00:49:03 +0300

    add | flag url extension function

--- app/src/main/java/com/tsci/electioncountdown/data/model/CountryItem.kt
+++ app/src/main/java/com/tsci/electioncountdown/data/model/CountryItem.kt
@@ -1,6 +1,7 @@
 package com.tsci.electioncountdown.data.model
 
 import com.google.gson.annotations.SerializedName
+import com.tsci.electioncountdown.common.Constants
 
 data class CountryItem(
     @SerializedName("Country")
@@ -14,3 +15,6 @@ data class CountryItem(
     @SerializedName("Next Election")
     val nextElection: String
 )
+internal fun CountryItem.getFlagImageUrl(): String {
+    return Constants.BASE_URL + "/" + this.countryCode
+}
