b111017e290e2e2e993e715dd7864ec67d7ab5b0
Burak-Tasci <buraaktascii@outlook.com>
2022-04-28 00:53:03 +0300

    add | spinner component functionality

--- app/src/main/java/com/tsci/electioncountdown/presentation/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/tsci/electioncountdown/presentation/ui/settings/SettingsFragment.kt
@@ -1,24 +1,83 @@
 package com.tsci.electioncountdown.presentation.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tsci.electioncountdown.R
+import android.widget.AdapterView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager
+import com.tsci.electioncountdown.databinding.SettingsFragmentBinding
+import com.tsci.electioncountdown.presentation.ui.MainViewModel
+import com.tsci.electioncountdown.presentation.ui.MainViewModel.Companion.COUNTRY_NAME
+import com.tsci.electioncountdown.presentation.ui.countdown.CountdownFragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+private const val TAG = "SettingsFragment.kt"
+
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    private var _binding: SettingsFragmentBinding? = null
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+    ): View {
+        _binding = SettingsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        binding.countrySpinner.adapter = CountryArrayAdapter(
+            requireContext(),
+            viewModel
+        )
+//        binding.countrySpinner.selectedItem =
+
+        binding.countrySpinner.setSelection(
+            viewModel.getCountryItemByName(
+                PreferenceManager.getDefaultSharedPreferences(requireContext()).getString(COUNTRY_NAME, null).toString()
+            )!!.id.toInt() - 1
+        )
+
+        binding.countrySpinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View,
+                position: Int,
+                id: Long
+            ) {
+                viewModel.countryItem.value = viewModel.database[position]
+                PreferenceManager.getDefaultSharedPreferences(context!!)
+                    .edit()
+                    .putString(COUNTRY_NAME, viewModel.countryItem.value!!.country)
+                    .apply()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                viewModel.countryItem.value = viewModel.getCountryItemByName(
+                    PreferenceManager.getDefaultSharedPreferences(context!!).getString(
+                        COUNTRY_NAME, null).toString()
+                )!!
+
+            }
+        }
     }
 
 }
